f4b15a6fa13db2a87592ff33cccb2f8069d5bbfc
Adrian <leongadrian36@gmail.com>
2020-03-16 22:54:57 +0800

    added data

---
+++ data.php
@@ -0,0 +1,148 @@
+<?
+$product = array();
+$product[] = array(
+	"id" => 1,
+	'name' => "Young Woman Wearing Dress",
+	"old_price" => "120.00",
+	"new_price" => "100.00",
+	"img" => "../img/product/p1.jpg",
+);
+
+$product[] = array(
+	"id" => 1,
+	'name' => "Young Woman Wearing Dress",
+	"old_price" => "120.00",
+	"new_price" => "80.00",
+	"img" => "../img/product/p2.jpg",
+);
+
+$product[] = array(
+	"id" => 1,
+	'name' => "Young Woman Wearing Dress",
+	"old_price" => "",
+	"new_price" => "1.00",
+	"img" => "../img/product/p3.jpg",
+);
+
+$product[] = array(
+	"id" => 1,
+	'name' => "Young Woman Wearing Dress",
+	"old_price" => "",
+	"new_price" => "50.00",
+	"img" => "../img/product/p4.jpg",
+);
+
+$product[] = array(
+	"id" => 1,
+	'name' => "Young Woman Wearing Dress",
+	"old_price" => "",
+	"new_price" => "100.00",
+	"img" => "../img/product/p5.jpg",
+);
+
+$product[] = array(
+	"id" => 1,
+	'name' => "Young Woman Wearing Dress",
+	"old_price" => "",
+	"new_price" => "180.00",
+	"img" => "../img/product/p6.jpg",
+);
+
+$product[] = array(
+	"id" => 1,
+	'name' => "Adidas New hammer Sole for Sport Sho",
+	"old_price" => "",
+	"new_price" => "180.00",
+	"img" => "../img/product/p7.jpg",
+);
+
+$product[] = array(
+	"id" => 1,
+	'name' => "Adidas New hammer Sole for Sport Shoe",
+	"old_price" => "",
+	"new_price" => "8.00",
+	"img" => "../img/product/p8.jpg",
+);
+
+
+$testimony = array();
+$testimony[] = array(
+	'id' => 1,
+	'name' => 'Roger Scott',
+	'position' => 'Customer',
+	'img' => '../images/person_1.jpg',
+	'data' => 'A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.'
+);
+$testimony[] = array(
+	'id' => 1,
+	'name' => 'Roger Scott',
+	'position' => 'Customer',
+	'img' => '../images/person_2.jpg',
+	'data' => 'A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.'
+);
+$testimony[] = array(
+	'id' => 1,
+	'name' => 'Roger Scott',
+	'position' => 'Customer',
+	'img' => '../images/person_3.jpg',
+	'data' => 'A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.'
+);
+$testimony[] = array(
+	'id' => 1,
+	'name' => 'Roger Scott',
+	'position' => 'Customer',
+	'img' => '../images/person_4.jpg',
+	'data' => 'A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.'
+);
+$testimony[] = array(
+	'id' => 1,
+	'name' => 'Roger Scott',
+	'position' => 'Customer',
+	'img' => '../images/person_1.jpg',
+	'data' => 'A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.'
+);
+
+
+$blog = array();
+$blog[] = array(
+	'id' => 1,
+	'img' => '../images/image_1.jpg',
+	'name' => 'Even the all-powerful Pointing has no control about the blind texts',
+	'cdt' => '2018-12-06',
+	'author' => 'Admin'
+);
+$blog[] = array(
+	'id' => 2,
+	'img' => '../images/image_2.jpg',
+	'name' => 'Even the all-powerful Pointing has no control about the blind texts',
+	'cdt' => '2018-12-06',
+	'author' => 'Admin'
+);
+$blog[] = array(
+	'id' => 3,
+	'img' => '../images/image_3.jpg',
+	'name' => 'Even the all-powerful Pointing has no control about the blind texts',
+	'cdt' => '2018-12-06',
+	'author' => 'Admin'
+);
+$blog[] = array(
+	'id' => 4,
+	'img' => '../images/image_4.jpg',
+	'name' => 'Even the all-powerful Pointing has no control about the blind texts',
+	'cdt' => '2018-12-06',
+	'author' => 'Admin'
+);
+$blog[] = array(
+	'id' => 5,
+	'img' => '../images/image_5.jpg',
+	'name' => 'Even the all-powerful Pointing has no control about the blind texts',
+	'cdt' => '2018-12-06',
+	'author' => 'Admin'
+);
+$blog[] = array(
+	'id' => 6,
+	'img' => '../images/image_6.jpg',
+	'name' => 'Even the all-powerful Pointing has no control about the blind texts',
+	'cdt' => '2018-12-06',
+	'author' => 'Admin'
+);
