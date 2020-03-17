1afaf87a6abb932012fd787fae59ee921da82710
Bounx <bounx@KL-iMac1406-START.local>
2020-03-18 00:16:52 +0800

    product filterbar

---
+++ product-filterbar.php
@@ -0,0 +1,28 @@
+<!-- Start Filter Bar -->
+<div class="filter-bar d-flex flex-wrap align-items-center">
+	<div class="sorting">
+		<select>
+			<option value="1">Price: Low to High</option>
+			<option value="1">Price: High to Low</option>
+			<option value="1">Newest</option>
+			<option value="1">Oldest</option>
+		</select>
+	</div>
+	<div class="sorting mr-auto">
+		<select>
+			<option value="12">Show 12</option>
+			<option value="24">Show 24</option>
+			<option value="36">Show 36</option>
+		</select>
+	</div>
+	<div class="pagination">
+		<a href="#" class="prev-arrow"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></a>
+		<a href="#" class="active">1</a>
+		<a href="#">2</a>
+		<a href="#">3</a>
+		<a href="#" class="dot-dot"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a>
+		<a href="#">6</a>
+		<a href="#" class="next-arrow"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+	</div>
+</div>
+<!-- End Filter Bar -->
