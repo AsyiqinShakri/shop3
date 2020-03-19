d5859242e093c8559d2f5fddaeccf3b98461b127
Adrian <leongadrian36@gmail.com>
2020-03-19 14:55:44 +0800

    added product review

---
+++ product-review.php
@@ -0,0 +1,68 @@
+<div class="row total_rate justify-content-between">
+	<div class="col-6">
+		<div class="box_total">
+			<h5>Overall</h5>
+			<h4>4.0</h4>
+			<h6>(03 Reviews)</h6>
+		</div>
+	</div>
+	<div class="col-6">
+		<div class="rating_list text-center">
+			<h3>Based on 3 Reviews</h3>
+			<ul class="list">
+				<li><a href="#">5 Star <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i> 01</a></li>
+				<li><a href="#">4 Star <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i> 01</a></li>
+				<li><a href="#">3 Star <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i> 01</a></li>
+				<li><a href="#">2 Star <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i> 01</a></li>
+				<li><a href="#">1 Star <i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i> 01</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<div class="review_list">
+	<div class="review_item">
+		<div class="media">
+			<div class="media-body">
+				<h4>Blake Ruiz</h4>
+				<i class="fa fa-star"></i>
+				<i class="fa fa-star"></i>
+				<i class="fa fa-star"></i>
+				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>
+		</div>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+			dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+			commodo</p>
+	</div>
+	<div class="review_item">
+		<div class="media">
+			<div class="media-body">
+				<h4>Blake Ruiz</h4>
+				<i class="fa fa-star"></i>
+				<i class="fa fa-star"></i>
+				<i class="fa fa-star"></i>
+				<i class="fa fa-star"></i>
+				<i class="fa fa-star"></i>
+			</div>
+		</div>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+			dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+			commodo</p>
+	</div>
+	<div class="review_item">
+		<div class="media">
+			<div class="media-body">
+				<h4>Blake Ruiz</h4>
+				<i class="fa fa-star"></i>
+				<i class="fa fa-star"></i>
+				<i class="fa fa-star"></i>
+				<i class="fa fa-star"></i>
+				<i class="fa fa-star"></i>
+			</div>
+		</div>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+			dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+			commodo</p>
+	</div>
+</div>
