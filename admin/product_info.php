db5bd014f1d64d9a35ad8ac7b667fbe87e2b1044
Adrian <leongadrian36@gmail.com>
2020-03-19 14:41:38 +0800

    added code and statuses

--- admin/product_info.php
+++ admin/product_info.php
@@ -10,11 +10,17 @@ $p_flux = array(
 	"name",			"Name",				"text_5",			"req",
 	"cat_id",		"Category",			"lkcbo_cat",		"",
 	"____",			"Info",				"split",			"",
-	"ishomebanner",	"Home Banner",		"cbx",				"",
-	"ishide",		"Hidden",			"cbx",				"",
 	"qty",			"Quantity",			"text",				"int",
-	"price",		"Price",			"text",				"reqdbl",
+	"price2",		"Old Price",		"text",				"dbl",
+	"price",		"New Price",		"text",				"reqdbl",
 	"weight",		"Weight",			"text",				"reqdbl",
+	"____",			"Status",			"split",			"",
+	"ishomebanner",	"Home Banner",		"cbx",				"",
+	"islatest",		"Latest",			"cbx",				"",
+	"isupcoming",	"Upcoming",			"cbx",				"",
+	"isexclu",		"Exclusive",		"cbx",				"",
+	"isdeal",		"Deal",				"cbx",				"",
+	"ishide",		"Hidden",			"cbx",				"",
 	"____",			"SEO Data",			"split",			"",
 	"keyword",		"Keywords",			"text",				"",
 	"meta_desc",	"Meta Description",	"memo",				"",
@@ -52,7 +58,49 @@ $acceptUploadExt = array("jpg", "png", "jpeg");
 <form name="f" action="<?= $site_url . "/" . $this_file ?>" method="post" <?= ($multipart ? "enctype=\"multipart/form-data\"" : "") ?>>
 	<div class="card-body">
 		<? require $libbase . "mergei.lib"; ?>
-		<? require $libbase . "sqlbasic.lib"; ?>
+		<? //require $libbase . "sqlbasic.lib"; 
+		?>
+		<?
+		require $libbase . "preparam.lib";
+
+		if ($submitted) {
+			require_once $libbase . "validation.lib";
+			if ($multipart) {
+				for ($i = 0; $i < sizeof($upload); $i++) {
+					if ($validated) {
+						$upload_path = "ul_" . $upload[$i];
+						$target_field = $upload[$i];
+						$upload_label = $upload_lbl[$i];
+						require $libbase . "fileupload.lib";
+						if ($error != "") {
+							$validated = 0;
+							break;
+						}
+					} else {
+						break;
+					}
+				}
+			}
+			if ($validated) {
+				if ($id == "") {
+					require $libbase . "sqlinsert.lib";
+				} else {
+					require $libbase . "sqlupdate.lib";
+				}
+				if ($p["code"] == "(auto)") {
+					$code = "P-" . pad($id, 0, 6);
+					$sql = "UPDATE " . $sql_table . " SET code = '" . $code . "' WHERE id = " . $id;
+					mq($sql);
+				}
+				set("update", "Record successfully " . $action);
+				go($next_file);
+			}
+		} else if ($id !== "") {
+			require $libbase . "sqlselect.lib";
+		} else {
+			$p["code"] = "(auto)";
+		}
+		?>
 		<? for ($i = 0; $i < sizeof($p_list); $i++) { ?>
 			<? require $libbase . "frmfield.lib"; ?>
 		<? } ?>
