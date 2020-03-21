59128328f2049f2ca649f7625cacaccb2572b696
Adrian <leongadrian36@gmail.com>
2020-03-21 18:08:00 +0800

    login/register function

--- login.php
+++ login.php
@@ -1,24 +1,26 @@
 <? $breadcrumb = 1; ?>
 <? $page = "Login/Register"; ?>
 <? include "header.php"; ?>
-
+<? if (!empty($user)) go($site_url . "/category.php"); ?>
+<? $rusr = $usr = "adrian@rubysoft.com.my"; ?>
+<? $rpwd = $rcpwd = $pwd = "123456"; ?>
 
 <!--================Login Box Area =================-->
 <section class="login_box_area section_gap">
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-6">
-				<div class="login_form_inner">
+				<div class="login_form_inner reg_form">
 					<h3>Join Us</h3>
-					<form class="row login_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
+					<form class="row register_form" action="" method="post" id="registerForm" novalidate="novalidate">
 						<div class="col-md-12 form-group">
-							<input type="text" class="form-control" id="username" name="username" placeholder="Username" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'">
+							<input type="text" class="form-control" id="remail" name="remail" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'" value="<?= $rusr ?>">
 						</div>
 						<div class="col-md-12 form-group">
-							<input type="text" class="form-control" id="password" name="password" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
+							<input type="password" class="form-control" id="rpassword" name="rpassword" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'" value="<?= $rpwd ?>">
 						</div>
 						<div class="col-md-12 form-group">
-							<input type="text" class="form-control" id="cpassword" name="cpassword" placeholder="Confirm Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Confirm Password'">
+							<input type="password" class="form-control" id="rcpassword" name="rcpassword" placeholder="Confirm Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Confirm Password'" value="<?= $rcpwd ?>">
 						</div>
 						<div class="col-md-12 form-group">
 							<button type="submit" value="submit" class="primary-btn">Register Now</button>
@@ -28,18 +30,18 @@
 			</div>
 			<div class="col-lg-6">
 				<div class="login_form_inner">
-					<h3>Log in to enter</h3>
-					<form class="row login_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
+					<h3>Log in</h3>
+					<form class="row login_form" action="" method="post" id="loginForm" novalidate="novalidate">
 						<div class="col-md-12 form-group">
-							<input type="text" class="form-control" id="name" name="name" placeholder="Username" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'">
+							<input type="email" class="form-control" id="email" name="email" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'" value="<?= $usr ?>">
 						</div>
 						<div class="col-md-12 form-group">
-							<input type="text" class="form-control" id="name" name="name" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
+							<input type="password" class="form-control" id="password" name="password" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'" value="<?= $pwd ?>">
 						</div>
 						<div class="col-md-12 form-group">
 							<div class="creat_account">
-								<input type="checkbox" id="f-option2" name="selector">
-								<label for="f-option2">Keep me logged in</label>
+								<input type="checkbox" id="rememberMe" name="rememberMe">
+								<label for="rememberMe">Keep me logged in</label>
 							</div>
 						</div>
 						<div class="col-md-12 form-group">
