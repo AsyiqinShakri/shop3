9d6eb4704b6ca303812a6b164eabf6b11d55e777
Adrian <leongadrian36@gmail.com>
2020-03-25 13:22:21 +0800

    site url

--- admin/common.php
+++ admin/common.php
@@ -18,7 +18,7 @@ $company_name = "Company Name";
 $mail_sender = "noreply@adrian.com";
 $mail_recipient = "info@adrian.com";
 $site_url = "http://adrianleong.ddns.net/shop3/admin";	//without www.
-$site_url = "http://localhost/shop3/admin";	//without www
+// $site_url = "http://localhost/shop3/admin";	//without www
 
 $libbase = "lib/";
 $mce_src_url = "assets/tinymce/js/tinymce";
