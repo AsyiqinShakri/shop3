270852307d4f4f5c050507b5e6f26b073be537ae
Adrian <leongadrian36@gmail.com>
2020-03-25 20:13:01 +0800

    update style

--- components/product-deal.php
+++ components/product-deal.php
@@ -5,7 +5,7 @@
 <? $new_price = $new_price; ?>
 <div class="single-related-product d-flex">
 	<a href="<?= $link ?>" class="stretched-link"><img src="<?= $img ?>" alt="<?= $name ?>" class=""></a>
-	<div class="desc w-100 text-center text-md-left d-flex flex-column justify-content-center">
+	<div class="desc w-100 text-center text-md-left d-flex flex-column">
 		<a href="<?= $link ?>" class="title small"><?= $name ?></a>
 		<div class="price">
 			<h5 class="small mb-0"><?= $currency ?><?= dfd($new_price) ?></h5>
